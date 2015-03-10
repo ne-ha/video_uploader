659e4cea1c8e2f4125221d8c98ebe5224feef4cb
NEHA SUWAl <neha@jyaasa.com>
2015-03-10 10:26:46 +0545

    Defined actions index, new and show.

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,12 +3,15 @@ class VideosController < ApplicationController
   before_action :authenticate_user!
   
   def index
+    @videos = current_user.videos
   end
 
   def show
+    @video = current_user.videos.find(params[:id])
   end
 
   def new
+    @video = current_user.my_videos.new
   end
 
   def edit
