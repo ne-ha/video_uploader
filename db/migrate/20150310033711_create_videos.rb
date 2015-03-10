dfd957439e634df339d86548f13ab15f4cde5364
NEHA SUWAl <neha@jyaasa.com>
2015-03-10 09:29:26 +0545

    Added user_id in vidoes table.

--- db/migrate/20150310033711_create_videos.rb
+++ db/migrate/20150310033711_create_videos.rb
@@ -1,6 +1,7 @@
 class CreateVideos < ActiveRecord::Migration
   def change
     create_table :videos do |t|
+      t.integer :user_id
       t.integer :category_id
       t.string :name
       t.text :description
