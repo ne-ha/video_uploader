50a877ca0d37f38c288a0c693b9e07e08841df0a
NEHA SUWAl <neha@jyaasa.com>
2015-03-10 11:08:11 +0545

    Destroyed migration CreateVidoesUser.

--- db/migrate/20150310035445_create_vidoes_users.rb
+++
@@ -1,8 +0,0 @@
-class CreateVidoesUsers < ActiveRecord::Migration
-  def change
-    create_table :vidoes_users do |t|
-      t.belongs_to :user, index: true
-      t.belongs_to :video, index: true
-    end
-  end
-end
