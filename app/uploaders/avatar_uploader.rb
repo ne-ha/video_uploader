e55add0b7f225b9f7bd7a60ab3137873f51879ea
NEHA SUWAl <neha@jyaasa.com>
2015-03-10 15:49:11 +0545

    Destroyed all videos

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -6,6 +6,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
+  process :resize_to_fit => [200, 200]
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -15,6 +16,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_white_list
+    %w(mov avi mp4 mkv wmv mpg)
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
