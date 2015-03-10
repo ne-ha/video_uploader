99f4be910d8579236f33ad759f16385e4f7b02bd
NEHA SUWAl <neha@jyaasa.com>
2015-03-10 09:41:25 +0545

    Ran db migrate after generating table videos and users which is a join table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150310033711) do
+ActiveRecord::Schema.define(version: 20150310035445) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -39,5 +39,13 @@ ActiveRecord::Schema.define(version: 20150310033711) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "vidoes_users", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "video_id"
+  end
+
+  add_index "vidoes_users", ["user_id"], name: "index_vidoes_users_on_user_id"
+  add_index "vidoes_users", ["video_id"], name: "index_vidoes_users_on_video_id"
 
 end
