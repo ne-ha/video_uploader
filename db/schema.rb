e6b70845429f170f3ecbbfa1953479812a41bd72
NEHA SUWAl <neha@jyaasa.com>
2015-03-10 13:29:45 +0545

    Destroyed music model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150310052410) do
+ActiveRecord::Schema.define(version: 20150310072513) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "musics", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
