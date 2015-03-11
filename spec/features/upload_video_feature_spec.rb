699af28a287d9fcc0b6ae11245f2eb430cb4baa1
NEHA SUWAl <neha@jyaasa.com>
2015-03-11 16:37:03 +0545

    this test check if the file is uploaded or not.

--- spec/features/upload_video_feature_spec.rb
+++ spec/features/upload_video_feature_spec.rb
@@ -7,8 +7,10 @@ describe "user", :type=> :feature, :js => true do
 
   before :each do
     User.destroy_all
+    Category.destroy_all
     user = FactoryGirl.create(:user)
     login_as(user, :scope => :user)
+
     FactoryGirl.create(:category)
   end
 
@@ -22,10 +24,11 @@ describe "user", :type=> :feature, :js => true do
     within(".new") do
       fill_in  'Name', :with => 'test'
       fill_in  'Description', :with => 'secret'
-      select "Games" , :from=> "video_Category"
+      select "Music" , :from=> "video_category_id"
+      attach_file('video[avatar]',"#{Rails.root}/public/uploads/video/avatar/Minecraft_ ROCK PAPER SCISSORS OF DEATH GAME - Animation.mp4")
     end
     click_button "Upload"
-    expect(page).to have_content "Video uploaded successfully."
+    expect(page).to have_content "Video has been uploaded."
   end
 
 end
