01fe45f0b5b12d981dc4f1a50f350d7c052e5ccb
NEHA SUWAl <neha@jyaasa.com>
2015-03-11 10:36:37 +0545

    Ran db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -19,11 +19,6 @@ ActiveRecord::Schema.define(version: 20150310143333) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "musics", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "shares", force: :cascade do |t|
     t.string   "share_to"
     t.datetime "created_at", null: false
@@ -41,6 +36,10 @@ ActiveRecord::Schema.define(version: 20150310143333) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
