24b6a21f98d8c9d0908ed3f910b401cdf7591f4d
NEHA SUWAl <neha@jyaasa.com>
2015-03-11 13:15:20 +0545

    For confirmation and set password after.

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,18 +1,22 @@
-class ConfirmationsController < Devise::ConfirmationsController
-  
-  def show
-    @user = User.find_by_confirmation_token(params[:confirmation_token])
-  end
+# class ConfirmationsController < Devise::ConfirmationsController
 
-  def confirm
-    @user = User.find_by_confirmation_token(params[:user][:confirmation_token])
-    if @user.update_attributes(params[:user]) and @user.password_match?
-      @user = User.confirm_by_token(@user.confirmation_token)
-      set_flash_message :notice, :confirmed
-      sign_in_and_redirect("user", @user)
-    else
-      render :show
-    end
-  end
+#   def show
+#     @user = User.find_by_confirmation_token(params[:confirmation_token])
+#   end
 
-end
+#   def confirm
+#     @user = User.find_by_confirmation_token(params[:user][:confirmation_token])
+#     if @user.update_attributes(user_params) and @user.password_match?
+#       @user = User.confirm_by_token(@user.confirmation_token)
+#       set_flash_message :notice, :confirmed
+#       sign_in_and_redirect(:user, @user)
+#     else
+#       render :show
+#     end
+#   end
+
+#   private
+#     def user_params
+#       params.require(:user).permit(:password , :password_confirmation)
+#     end
+# end
