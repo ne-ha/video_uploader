df2ed1b761b722848c4c5aa37d611f5b8b2588a1
NEHA SUWAl <neha@jyaasa.com>
2015-03-11 16:09:56 +0545

    Added validation for name

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,6 +3,8 @@ class Video < ActiveRecord::Base
   has_and_belongs_to_many :users
   belongs_to :category
 
+  validates :name , :presence => true
+
   mount_uploader :avatar, AvatarUploader
   
 end
