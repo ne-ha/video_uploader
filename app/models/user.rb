d80db7a87c2151688c08ff528e72f78757771105
NEHA SUWAl <neha@jyaasa.com>
2015-03-11 11:39:07 +0545

    Added method password_match

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,11 @@ class User < ActiveRecord::Base
   has_many :my_videos, class_name: "Video"
 
   has_and_belongs_to_many :videos
+
+  def password_match?
+    self.errors[:password] << 'must be provided' if password.blank?
+    self.errors[:password] << 'and confirmation do not match' if password != password_confirmation
+    password == password_confirmation and !password.blank?
+  end
   
 end
