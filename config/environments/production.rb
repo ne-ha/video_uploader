2cbf5084a3b898f1d293b5079d6be390e3479995
NEHA SUWAl <neha@jyaasa.com>
2015-03-11 10:36:02 +0545

    Added configuration for devise to send confirmation email.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.log_level = :warn
+
+  config.action_mailer.default_url_options = {:host => 'testtalkruby@gmail.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "127.0.0.1",
+    :port    => 25,
+    :domain  => 'testtalkruby@gmail.com'
+  }
 end
