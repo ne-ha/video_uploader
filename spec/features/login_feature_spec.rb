ef06479c719acaac72de37108d9026ef629db7ed
NEHA SUWAl <neha@jyaasa.com>
2015-03-11 09:28:54 +0545

    Added test for checking login process

---
+++ spec/features/login_feature_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe "log in", :type=> :feature do
+
+  before :each do
+    User.destroy_all
+    user = FactoryGirl.create(:user)
+  end
+
+  it "should have an email" do
+    user = FactoryGirl.create(:user, email:"user@example.com")
+    expect(user.email).to eq("user@example.com")
+  end
+
+  it "sigin in " do
+    visit "/users/sign_in"
+    within("#new_user") do
+      fill_in  'Email', :with => 'test@example.com'
+      fill_in  'Password', :with => 'secretpassword'
+    end
+    click_button "Log in"
+    expect(page).to have_content "Signed in successfully."
+  end
+  
+end
