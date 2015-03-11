7d480ad712d6af77ed1eb68c6b457ef3e76d5f77
NEHA SUWAl <neha@jyaasa.com>
2015-03-11 16:36:44 +0545

    This test checks if the video has name of not.

---
+++ spec/features/check_validation_feature_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+include Warden::Test::Helpers
+Warden.test_mode!
+
+describe "user", :type=> :feature, :js => true do
+
+  before :each do
+    User.destroy_all
+    Category.destroy_all
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user)
+
+    FactoryGirl.create(:category)
+  end
+
+  after :each do
+    Warden.test_reset!
+  end
+
+  it "should not abe able to upload if name is empty" do
+    visit root_path
+    click_link "Upload Video"
+    within(".new") do
+      fill_in  'Name', :with => ''
+      fill_in  'Description', :with => 'secret'
+      select "Music" , :from=> "video_category_id"
+    end
+    click_button "Upload"
+    expect(page).to have_content "Name cannot be empty."
+  end
+
+end
