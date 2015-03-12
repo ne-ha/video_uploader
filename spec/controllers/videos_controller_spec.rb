7ad22ac870917c6b2e7842bf63ac49620f844af7
NEHA SUWAl <neha@jyaasa.com>
2015-03-12 11:37:16 +0545

    Added test to upload video.

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -5,6 +5,7 @@ describe VideosController do
   before :each do
     User.destroy_all
     user = FactoryGirl.create(:user)
+    @category = FactoryGirl.create(:category)
     sign_in :user, user
   end
 
@@ -14,6 +15,12 @@ describe VideosController do
         get :index
         expect(response).to render_template("videos/index")
       end
+    end
+
+    it "should upload video" do
+      expect{ post :create , video: {name: 'Minecraft', description: 'This is the video of minecraft',
+        category_id: @category.id, 
+        avatar: Rack::Test::UploadedFile.new(File.join(Rails.root, 'public', 'uploads', 'video', 'avatar', 'Minecraft_ ROCK PAPER SCISSORS OF DEATH GAME - Animation.mp4'))}}.to change{Video.count}.by(1)
     end
   end
 end
