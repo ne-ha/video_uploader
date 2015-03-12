88a0b8cedc563730d9fe4bd05cf2b857c3e2c052
NEHA SUWAl <neha@jyaasa.com>
2015-03-12 13:18:28 +0545

    Added spec to check  if the view renders new template.

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -4,9 +4,9 @@ describe VideosController do
 
   before :each do
     User.destroy_all
-    user = FactoryGirl.create(:user)
+    @user = FactoryGirl.create(:user)
     @category = FactoryGirl.create(:category)
-    sign_in :user, user
+    sign_in :user, @user
   end
 
   describe 'GET index' do
@@ -16,11 +16,23 @@ describe VideosController do
         expect(response).to render_template("videos/index")
       end
     end
+  end
 
     it "should upload video" do
       expect{ post :create , video: {name: 'Minecraft', description: 'This is the video of minecraft',
         category_id: @category.id, 
         avatar: Rack::Test::UploadedFile.new(File.join(Rails.root, 'public', 'uploads', 'video', 'avatar', 'Minecraft_ ROCK PAPER SCISSORS OF DEATH GAME - Animation.mp4'))}}.to change{Video.count}.by(1)
     end
-  end
+
+    it "should delete a video" do
+      @video = FactoryGirl.create(:video)
+      @video.user_id = @user.id
+      @video.save
+      expect{delete :destroy , :id =>@video}.to change{Video.count}.by(-1)
+    end
+
+    it "should render new template" do
+      get :new
+      expect(response).to render_template("videos/new")
+    end
 end
