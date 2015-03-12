3f4524060bd334869bdb51327ff90f79fdb7db3e
NEHA SUWAl <neha@jyaasa.com>
2015-03-12 09:07:36 +0545

    Added validate presence for avatar

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,10 +1,9 @@
 class Video < ActiveRecord::Base
-
   has_and_belongs_to_many :users
   belongs_to :category
 
   validates :name , :presence => true
+  validates :avatar , :presence => true
 
   mount_uploader :avatar, AvatarUploader
-  
 end
