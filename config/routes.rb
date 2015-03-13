49921ee65c007ba093881ed342f27a7372e90acb
NEHA SUWAl <neha@jyaasa.com>
2015-03-13 09:52:13 +0545

    Added confirmation controller inside devise user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  
-  devise_for :users
+  devise_for :users, :controllers => {:confirmations => 'confirmations'}
+
+  devise_scope :user do
+    patch "/confirm" => "confirmations#confirm"
+  end
   
   resources :users
 
